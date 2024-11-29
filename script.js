84ae78bb36d91bc4e93c19445c6926830dc38d12
Nawre <erwan.arnoult@ensea.fr>
2024-11-29 09:20:25 +0100

    swap

--- script.js
+++ script.js
@@ -1,85 +1,86 @@
-{
-	title: "Tekken",
-	year: 1994,
-	imageUrl:
-		"https://cdn.dashfight.com/bcf6a9046a9ea4c1070d4aedb2981103c978a704.png",
-	id: 1,
-},
-{
-	title: "Minecraft",
-	year: 2009,
-	imageUrl:
-		"https://m.media-amazon.com/images/I/61smNbXSW1L._AC_UF1000,1000_QL80_.jpg",
-	id: 2,
-},
-{
-	title: "Elden Ring",
-	year: 2022,
-	imageUrl:
-		"https://pic.clubic.com/v1/images/1934271/raw?fit=smartCrop&width=1200&height=675&hash=e7519a9577a2b7291fa26880bb22bed6740836be",
-	id: 3,
-},
-{
-	title: "Street Fighter V",
-	year: 2015,
-	imageUrl:
-		"https://gaming-cdn.com/images/products/671/orig/street-fighter-v-pc-jeu-steam-cover.jpg?v=1662539255",
-	id: 4,
-},
-{
-	title: "Half Life 2",
-	year: 2004,
-	imageUrl:
-		"https://gaming-cdn.com/images/products/2284/orig/half-life-2-pc-mac-game-steam-cover.jpg?v=1650555068",
-	id: 5,
-},
-{
-	title: "Skyrim",
-	year: 2011,
-	imageUrl:
-		"https://gaming-cdn.com/images/products/146/orig/the-elder-scrolls-v-skyrim-pc-jeu-steam-europe-cover.jpg?v=1661270991",
-	id: 6,
-},
+const gamesList = [
+	{
+		title: "Tekken",
+		year: 1994,
+		imageUrl:
+			"https://cdn.dashfight.com/bcf6a9046a9ea4c1070d4aedb2981103c978a704.png",
+		id: 1,
+	},
+	{
+		title: "Minecraft",
+		year: 2009,
+		imageUrl:
+			"https://m.media-amazon.com/images/I/61smNbXSW1L._AC_UF1000,1000_QL80_.jpg",
+		id: 2,
+	},
+	{
+		title: "Elden Ring",
+		year: 2022,
+		imageUrl:
+			"https://pic.clubic.com/v1/images/1934271/raw?fit=smartCrop&width=1200&height=675&hash=e7519a9577a2b7291fa26880bb22bed6740836be",
+		id: 3,
+	},
+	{
+		title: "Street Fighter V",
+		year: 2015,
+		imageUrl:
+			"https://gaming-cdn.com/images/products/671/orig/street-fighter-v-pc-jeu-steam-cover.jpg?v=1662539255",
+		id: 4,
+	},
+	{
+		title: "Half Life 2",
+		year: 2004,
+		imageUrl:
+			"https://gaming-cdn.com/images/products/2284/orig/half-life-2-pc-mac-game-steam-cover.jpg?v=1650555068",
+		id: 5,
+	},
+	{
+		title: "Skyrim",
+		year: 2011,
+		imageUrl:
+			"https://gaming-cdn.com/images/products/146/orig/the-elder-scrolls-v-skyrim-pc-jeu-steam-europe-cover.jpg?v=1661270991",
+		id: 6,
+	},
 ]
 
 function writeDom() {
-gamesList.forEach((game) => {
-	const articleContainer = document.querySelector(".row")
-	articleContainer.innerHTML += `<article class="col">
-						<div class="card shadow-sm">
-							<img src="${game.imageUrl}" alt="${game.title}" class="card-img-top" />
+	gamesList.forEach((game) => {
+		const articleContainer = document.querySelector(".row")
+		articleContainer.innerHTML += `<article class="col">
+							<div class="card shadow-sm">
+								<img src="${game.imageUrl}" alt="${game.title}" class="card-img-top" />
 
-							<div class="card-body">
-							<h3 class="card-title">${game.title}</h3>
-								<p class="card-text">
-									${game.year}
-								</p>
-								<div
-									class="d-flex justify-content-between align-items-center"
-								>
-									<div class="btn-group">
-										<button
-											type="button"
-											class="btn btn-sm btn-outline-secondary view"
-											data-bs-toggle="modal" data-bs-target="#exampleModal"
-											data-view-id="${game.id}"
-										>
-											View
-										</button>
-										<button
-											type="button"
-											class="btn btn-sm btn-outline-secondary edit"
-											data-bs-toggle="modal" data-bs-target="#exampleModal"
-											data-edit-id="${game.id}"
-										>
-											Edit
-										</button>
+								<div class="card-body">
+								<h3 class="card-title">${game.title}</h3>
+									<p class="card-text">
+										${game.year}
+									</p>
+									<div
+										class="d-flex justify-content-between align-items-center"
+									>
+										<div class="btn-group">
+											<button
+												type="button"
+												class="btn btn-sm btn-outline-secondary view"
+												data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                                data-view-id="${game.id}"
+											>
+												View
+											</button>
+											<button
+												type="button"
+												class="btn btn-sm btn-outline-secondary edit"
+												data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                                data-edit-id="${game.id}"
+											>
+												Edit
+											</button>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					</article>`
-})
+						</article>`
+	})
 }
 writeDom()
 
@@ -88,85 +89,85 @@ writeDom()
 
 /*const editButtons = document.querySelectorAll(".edit")
 editButtons.forEach((btn) => {
-btn.addEventListener("click", () => {
-	console.log("hello edit !!!")
-})
+	btn.addEventListener("click", () => {
+		console.log("hello edit !!!")
+	})
 })*/
 
 /*const editButtons = document.querySelectorAll(".edit")
 editButtons.forEach((btn) => {
-btn.addEventListener("click", (e) => {
-	console.log(e.target.getAttribute("data-edit-id"))
-})
+	btn.addEventListener("click", (e) => {
+		console.log(e.target.getAttribute("data-edit-id"))
+	})
 })*/
 
 const editButtons = document.querySelectorAll(".edit")
 editButtons.forEach((btn) => {
-btn.addEventListener("click", (e) => {
-	editModal(e.target.getAttribute("data-edit-id"))
-})
+	btn.addEventListener("click", (e) => {
+		editModal(e.target.getAttribute("data-edit-id"))
+	})
 })
 
 /*function editModal(gameId) {
-// console.log(gameId, gamesList)
-// Trouvez le jeu en fonction de son identifiant
-const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
-// passer une image comme corps du modal
-const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
-modifyModal("Mode Edition", modalBody)
+	// console.log(gameId, gamesList)
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	// passer une image comme corps du modal
+	const modalBody = `<h4>ajoutez un formulaire pour modifier le jeu ici</h4>`
+	modifyModal("Mode Edition", modalBody)
 }*/
 
 function modifyModal(modalTitle, modalBody) {
-// Écrir le nom du jeu dans le titre du modal
-document.querySelector(".modal-title").textContent = modalTitle
-document.querySelector(".modal-body").innerHTML = modalBody
+	// Écrir le nom du jeu dans le titre du modal
+	document.querySelector(".modal-title").textContent = modalTitle
+	document.querySelector(".modal-body").innerHTML = modalBody
 }
 
 const viewButtons = document.querySelectorAll(".view")
 viewButtons.forEach((btn) => {
-btn.addEventListener("click", (e) => {
-	viewModal(e.target.getAttribute("data-view-id"))
-})
+	btn.addEventListener("click", (e) => {
+		viewModal(e.target.getAttribute("data-view-id"))
+	})
 })
 
 function viewModal(gameId) {
-// console.log(gameId, gamesList)
-// Trouvez le jeu en fonction de son identifiant
-const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
-const modalBody = `<img src="${gamesList[result].imageUrl}" alt="${gamesList[result].title}" class="img-fluid" />`
-modifyModal(gamesList[result].title, modalBody)
+	// console.log(gameId, gamesList)
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	const modalBody = `<img src="${gamesList[result].imageUrl}" alt="${gamesList[result].title}" class="img-fluid" />`
+	modifyModal(gamesList[result].title, modalBody)
 }
 
 /*function editModal(gameId) {
-// console.log(gameId, gamesList)
-// Trouvez le jeu en fonction de son identifiant
-const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
-modifyModal("Mode Edition")
+	// console.log(gameId, gamesList)
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	modifyModal("Mode Edition")
 }*/
 
 function editModal(gameId) {
-// Trouvez le jeu en fonction de son identifiant
-const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
-// Injectez le formulaire dans le corps du modal
-fetch("from.html").then((data) => {
-	data.text().then((form) => {
-		// Modifiez le titre et le corps du modal
-		const selectedGame = gamesList[result]
-		modifyModal("Mode Edition", form)
-		modifyFom({
-			title: selectedGame.title,
-			year: selectedGame.year,
-			imageUrl: selectedGame.imageUrl,
+	// Trouvez le jeu en fonction de son identifiant
+	const result = gamesList.findIndex((game) => game.id === parseInt(gameId))
+	// Injectez le formulaire dans le corps du modal
+	fetch("from.html").then((data) => {
+		data.text().then((form) => {
+			// Modifiez le titre et le corps du modal
+			const selectedGame = gamesList[result]
+			modifyModal("Mode Edition", form)
+			modifyFom({
+				title: selectedGame.title,
+				year: selectedGame.year,
+				imageUrl: selectedGame.imageUrl,
+			})
 		})
 	})
-})
 }
 
 
 
 function modifyFom(gameData) {
-const form = document.querySelector("form")
-form.title.value = gameData.title
-form.year.value = gameData.year
-form.imageUrl.value = gameData.imageUrl
+	const form = document.querySelector("form")
+	form.title.value = gameData.title
+	form.year.value = gameData.year
+	form.imageUrl.value = gameData.imageUrl
 }
